62780d5d460944a006d1b375a7efc62475d343c1
teory01 <youremail@byupathway.edu>
2025-07-05 16:48:06 +0100

    made changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,6 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    // this is Exercise 1
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }

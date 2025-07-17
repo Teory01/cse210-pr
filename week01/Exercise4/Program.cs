89aec673c0972be240a3a0266cc593b2b6691a01
teory01 <youremail@byupathway.edu>
2025-07-17 18:28:51 +0100

    Make changes

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,67 @@
 using System;
 
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        //codes of line 10 - 31 for adding the sum of numbers in a List
+        int response = -1;
+
+        while (!(response == 0))
+        {
+            Console.Write("Enter a number: ");
+            string num = Console.ReadLine();
+            response = int.Parse(num);
+             
+        // int.TryParse(response, out int number); (Alternate form to convert)
+
+
+            if (response != 0)
+            {
+                numbers.Add(response);
+            }
+        }    
+
+        int total = 0;
+
+        //{ALTERNATIVE - BELOW ⬇⬇⬇⬇⬇}
+        // foreach (int number in numbers)
+        // {
+        //     total += number;
+        // }
+
+        foreach (int number in numbers)
+        {
+            total = total + number;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The Average is: {average}");
+
+
+        // Find the max number
+        int highest = -1; // or (int highest = numbers[0];)
+        int lowest = 999999; //or (lowest = numbers[0];)
+        foreach (int number in numbers)
+        {
+            if (number > highest)
+            {
+                highest = number;
+            }
+
+            if (number < lowest)
+            {
+                lowest = number;
+            }
+        }
+        Console.WriteLine($"The highest number is: {highest}");
+        Console.WriteLine($"The lowest number is: {lowest}");
+
     }
+
 }

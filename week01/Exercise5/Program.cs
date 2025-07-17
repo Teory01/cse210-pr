54d50d434a46a6aa94f31430cbdd0237c78f5426
teory01 <youremail@byupathway.edu>
2025-07-17 18:37:18 +0100

    Changes made

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,46 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquaredNumber(number);
+        DisplayResult(name, squared);
+    
+    }
+
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine("Welcome to the program! ");
+    }
+
+    static string PromptUserName ()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber ()
+    {
+        Console.Write("Please enter your favourite number: ");
+        string userNumber = Console.ReadLine();
+        int conv = int.Parse(userNumber);
+        return conv;
+    }
+
+    static int SquaredNumber (int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult (string name, int square)
+    {
+        Console.WriteLine($"Brother {name}, the square of your favourite number is: {square}");
     }
 }
